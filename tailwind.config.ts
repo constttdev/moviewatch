73f80e3585ee27dff0797d8f10c3a92528884bae
= <=>
2024-09-25 15:26:32 +0200

    removed content from svelte page and changed the tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,19 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				text: '#e1e9f5',
+				background: '#202733',
+				primary: '#8da8d8',
+				secondary: '#556887',
+				accent: '#417ee1',
+				bar: '#4D6B88'
+			},
+			fontFamily: {
+				inter: 'Inter'
+			}
+		}
 	},
 
 	plugins: []
