12c95767cb73724956fbac4dd798dd6c7a0904b8
= <=>
2024-09-25 15:19:17 +0200

    Added the text and the bars and added a inter font and changed the tailwind config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,13 @@
-<h1>Currently under construction</h1>
+<div
+	class="grid min-h-screen w-full place-content-center place-items-center items-center justify-center"
+>
+	<div class="bg-bar h-1 w-[1219px] rounded-sm shadow-2xl"></div>
+	<div class="bg-bar mt-2 h-1 w-[800px] rounded-sm shadow-2xl"></div>
+	<div class="bg-bar mt-2 h-1 w-[609px] rounded-sm shadow-2xl"></div>
+	<h1 class="text-text font-inter mb-20 mt-20 text-4xl font-bold">
+		We are currently under construction
+	</h1>
+	<div class="bg-bar h-1 w-[609px] rounded-sm shadow-2xl"></div>
+	<div class="bg-bar mt-2 h-1 w-[800px] rounded-sm shadow-2xl"></div>
+	<div class="bg-bar mt-2 h-1 w-[1219px] rounded-sm shadow-2xl"></div>
+</div>
