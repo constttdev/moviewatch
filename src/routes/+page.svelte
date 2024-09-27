f1a978033b48df1f468effcd9197c7eff84f4e95
= <=>
2024-09-27 17:42:42 +0200

    Changed the read me and the page svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,16 @@
 <div
 	class="grid min-h-screen w-full place-content-center place-items-center items-center justify-center"
 >
-	<div class="bg-bar h-1 w-[1219px] rounded-sm shadow-2xl"></div>
-	<div class="bg-bar mt-2 h-1 w-[800px] rounded-sm shadow-2xl"></div>
-	<div class="bg-bar mt-2 h-1 w-[609px] rounded-sm shadow-2xl"></div>
-	<h1 class="text-text font-inter mb-20 mt-20 flex gap-2 text-4xl">
+	<div class="h-1 w-[1219px] rounded-sm bg-bar shadow-2xl"></div>
+	<div class="mt-2 h-1 w-[800px] rounded-sm bg-bar shadow-2xl"></div>
+	<div class="mt-2 h-1 w-[609px] rounded-sm bg-bar shadow-2xl"></div>
+	<h1 class="mb-20 mt-20 flex gap-2 font-inter text-4xl text-text">
 		<p class="font-bold text-blue-500">MovieWatch</p>
-		is currently under construction
+		is currently decontinued see updates at<a href="https://discord.gg/uWB9TPkKpA"
+			>the official Discord server</a
+		>
 	</h1>
-	<div class="bg-bar h-1 w-[609px] rounded-sm shadow-2xl"></div>
-	<div class="bg-bar mt-2 h-1 w-[800px] rounded-sm shadow-2xl"></div>
-	<div class="bg-bar mt-2 h-1 w-[1219px] rounded-sm shadow-2xl"></div>
+	<div class="h-1 w-[609px] rounded-sm bg-bar shadow-2xl"></div>
+	<div class="mt-2 h-1 w-[800px] rounded-sm bg-bar shadow-2xl"></div>
+	<div class="mt-2 h-1 w-[1219px] rounded-sm bg-bar shadow-2xl"></div>
 </div>
